3ec5314e25c49f6dc0120aae00cc7f15684dd699
Emmerson <donnelem@oregonstate.edu>
2019-10-14 14:39:32 -0700

    J QUERY
    LINKED

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("mainHeader").onclick = function() {
       this.style.color = 'orange'
     }
+    $( document ).ready(function() {
+      $("HEADER").click(function() {
+        $("HEADER").fadeOut( "FAST", function() {
+        });
+      });
+    });
   })
